6fc7f52222f280f460db9d12e99eaaef92f23555
rgamevfx <ryangamevfx@gmail.com>
2026-01-21 23:17:44 -0500

    add video splice

--- __init__.py
+++ __init__.py
@@ -1,5 +1,5 @@
 """
-VACE Nodes - ComfyUI Custom Nodes
+VACE Nodes - ComfyUI Custom Nodes Collection
 
 A collection of nodes for VACE (Video Art Composition Engine) workflows.
 
@@ -7,9 +7,10 @@ Developer: Ryan Game
 Email: ryangamevfx@gmail.com
 Category: Vace/VFX
 
-Nodes:
-- VACE Inpaint Keyframe Insert: Insert keyframes at specific positions
-- Mask Range Doctor: Edit mask ranges with white/black values
+Nodes Included:
+- VACE Inpaint Keyframe Insert: Insert keyframes at specific frame positions
+- Mask Range Doctor: Edit mask ranges by setting frames to white/black values
+- VACE Video Splice: Splice two video sequences with frame offset control
 """
 
 from .vace_inpaint_keyframe_insert import NODE_CLASS_MAPPINGS as KEYFRAME_MAPPINGS
@@ -18,15 +19,20 @@ from .vace_inpaint_keyframe_insert import NODE_DISPLAY_NAME_MAPPINGS as KEYFRAME
 from .mask_range_doctor import NODE_CLASS_MAPPINGS as MASK_MAPPINGS
 from .mask_range_doctor import NODE_DISPLAY_NAME_MAPPINGS as MASK_DISPLAY
 
-# Merge the mappings from both nodes
+from .vace_video_splice import NODE_CLASS_MAPPINGS as SPLICE_MAPPINGS
+from .vace_video_splice import NODE_DISPLAY_NAME_MAPPINGS as SPLICE_DISPLAY
+
+# Merge the mappings from all nodes
 NODE_CLASS_MAPPINGS = {
     **KEYFRAME_MAPPINGS,
-    **MASK_MAPPINGS
+    **MASK_MAPPINGS,
+    **SPLICE_MAPPINGS
 }
 
 NODE_DISPLAY_NAME_MAPPINGS = {
     **KEYFRAME_DISPLAY,
-    **MASK_DISPLAY
+    **MASK_DISPLAY,
+    **SPLICE_DISPLAY
 }
 
 __all__ = ['NODE_CLASS_MAPPINGS', 'NODE_DISPLAY_NAME_MAPPINGS']
